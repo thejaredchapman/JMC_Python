2ea8cd3812be34fdcd430afca1b0c3957e3d9b75
Jared Chapman <thejaredchapman@gmail.com>
2019-09-10 16:27:20 -0500

    comedian_log
    Simple Log of comedians

---
+++ comedian_log.py
@@ -0,0 +1,8 @@
+#Log of Comedians
+class Comedian
+	def __init__(self, age, location, type, style, fun_fact)
+		self.age = age
+		self.location = location
+		self.type = type
+		self.style = style
+		self.fun_fact = fun fact
