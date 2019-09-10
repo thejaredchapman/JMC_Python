ddadf0f3dfb83921c548d844f2f8dc236c48e46c
Jared Chapman <thejaredchapman@gmail.com>
2019-09-10 16:04:14 -0500

    apple
    class object apple with attributes

---
+++ apple.py
@@ -0,0 +1,8 @@
+#creating an apple
+class Apple ():
+	def __init__(self, color, weight, stem_length, circumfrence):
+		self.color = color
+		self.weight = weight
+		self.stem_length = stem_length
+		self.circumfrence = circumfrence
+		
