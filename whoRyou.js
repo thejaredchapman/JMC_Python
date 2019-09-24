770de27be4d83f992b2bc2256e02ee2e8bbf0c7f
Jared Chapman <thejaredchapman@gmail.com>
2019-09-24 15:05:55 -0500

    Add files via upload
    javascript 1st file

---
+++ whoRyou.js
@@ -0,0 +1,8 @@
+var name
+if (name ==="Jared") {
+	alert("what's up Jared!");
+}	else {
+	alert("hmmm..don't know you so you should identify yourself")
+}	else if (name === "Avery"); {
+	alert("hey Avery, thanks for being in my life")
+}
